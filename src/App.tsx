6b834a64f750ee278a3585430a1b75d23b45a605
Kutub Udd <socialnetwork98710@gmail.com>
2024-03-15 03:51:03 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import CommingSoon from './CommingSoon';
+import Footer from './Footer';
+import NavVar from './NavBar';
+
+function App() {
+  return (
+    <>
+      <NavVar />
+      <CommingSoon />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/CommingSoon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommingSoon = () => {
+  return (
+    <div className='my-4 flex justify-center text-center h-screen'>
+      <h1 className='text-3xl font-medium animate-pulse  my-auto justify-center'>We're Comming Soon...</h1>
+    </div>
+  );
+};
+
+export default CommingSoon;
